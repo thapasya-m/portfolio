0ce1c19da096577f97f1d7b566e575c527de8eb6
thapasya <thapasya.murali@gmail.com>
2019-09-09 00:15:27 +0530

    feat: vertical parallax

--- src/App.js
+++ src/App.js
@@ -2,19 +2,18 @@ import React from 'react';
 import './App.css';
 import Header from './components/header/header';
 import Footer from './components/footer/footer';
-import image1 from './assets/images/image.png';
 
 function App() {
   return (
     <div className="App">
     <Header />
-      <div className="main" style={{color: "blue"}}>
-        <div className="image-wrapper">
-          <img src={image1} alt="thats me"/>
-        </div>
-        {/* <a href="https://balsamiq.cloud/s6di74y/pwdw20j">[WIREFRAME] 
-        Contents to be added in future deployments.</a> */}
+      
+      <div className="image-wrapper">
+        <div className="left-face"></div>
+        <div className="mid-line"></div>
+        <div className="right-face"></div>
       </div>
+      
     <Footer />
     </div>
   );
--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Footer = (props) => {
     return (
-        <footer style={{ position: 'absolute', bottom:'1px', right:'50%'}}>2019 thapasya m</footer>
+        <footer style={{ position: 'fixed', bottom:'1px', right:'50%'}}>2019 thapasya m</footer>
     );
 }
 export default Footer;
